95a61a7eda5b5ec2fef59c3eb08a125ebc1a5a4e
Kurt Madel <kmadel@cloudbees.com>
2019-04-25 10:16:23 -0400

    Update imageNameTag.groovy

--- vars/imageNameTag.groovy
+++ vars/imageNameTag.groovy
@@ -2,6 +2,6 @@
 def call(body) {
   tokens = "${env.JOB_NAME}".tokenize('/')
   env.IMAGE_REPO = env.repoOwner
-  env.IMAGE_NAME = tokens[tokens.size()-2]
+  env.IMAGE_NAME = "helloworld-nodejs"
   env.IMAGE_TAG = tokens[tokens.size()-1]
 }
