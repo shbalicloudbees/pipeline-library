318108a12b81df8acdccda58cc3b578863eefc5d
Kurt Madel <kmadel@cloudbees.com>
2019-06-02 08:05:14 -0400

    Update kubeDeploy.groovy

--- vars/kubeDeploy.groovy
+++ vars/kubeDeploy.groovy
@@ -19,7 +19,7 @@ def call(imageName, imageTag, githubCredentialId, repoOwner) {
           echo repoNotExists
           if(repoNotExists) {
           sh(script: '''
-              curl -H "Authorization: token $ACCESS_TOKEN" --data '{"name":"$envProdRepo"}' https://api.github.com/orgs/$repoOwner/repos
+              curl -H "Authorization: token $ACCESS_TOKEN" --data '{"name":$envProdRepo}' https://api.github.com/orgs/$repoOwner/repos
             ''')
           }
           //curl -H "Authorization: token ACCESS_TOKEN" --data '{"name":""}' https://api.github.com/orgs/ORGANISATION_NAME/repos
