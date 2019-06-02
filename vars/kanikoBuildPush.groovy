5e04bcb7b2cc181138eb814acd663f59417528a0
Kurt Madel <kmadel@cloudbees.com>
2019-06-02 06:35:23 -0400

    Update kanikoBuildPush.groovy

--- vars/kanikoBuildPush.groovy
+++ vars/kanikoBuildPush.groovy
@@ -6,11 +6,11 @@ def call(String imageName, String imageTag = env.BUILD_NUMBER, String gcpProject
   def podYaml = libraryResource 'podtemplates/dockerBuildPush.yml'
   podTemplate(name: 'kaniko', label: label, yaml: podYaml, inheritFrom: 'default-jnlp', nodeSelector: 'type=agent') {
     node(label) {
+      body()
       imageNameTag()
       gitShortCommit()
       def repoName = env.IMAGE_REPO.toLowerCase()
       container(name: 'kaniko', shell: '/busybox/sh') {
-        body()
         withEnv(['PATH+EXTRA=/busybox:/kaniko']) {
           sh """#!/busybox/sh
             /kaniko/executor -f ${pwd()}/${dockerFile} -c ${pwd()} --build-arg context=${repoName} --build-arg buildNumber=${BUILD_NUMBER} --build-arg shortCommit=${SHORT_COMMIT} --build-arg commitAuthor=${COMMIT_AUTHOR} -d ${dockerReg}/${repoName}:${BUILD_NUMBER}
