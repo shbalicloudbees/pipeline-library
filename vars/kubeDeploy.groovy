560aac1c6085d2f0652e69f9d3c6f74ab0e1fd44
Kurt Madel <kmadel@cloudbees.com>
2019-06-03 18:54:18 -0400

    Update kubeDeploy.groovy

--- vars/kubeDeploy.groovy
+++ vars/kubeDeploy.groovy
@@ -4,7 +4,7 @@ def call(imageName, imageTag, githubCredentialId, repoOwner) {
     def podYaml = libraryResource 'podtemplates/kubeDeploy.yml'
     def deployYaml = libraryResource 'k8s/basicDeploy.yml'
     def repoName = env.IMAGE_REPO.toLowerCase()
-    def envProdRepo = "environment_prod"
+    def envStagingRepo = "environment_staging"
     
     podTemplate(name: 'kubectl', label: label, yaml: podYaml) {
       node(label) {
@@ -14,18 +14,18 @@ def call(imageName, imageTag, githubCredentialId, repoOwner) {
           echo repoOwner
           echo envProdRepo
           def repoNotExists = sh(script: '''
-              curl -H "Authorization: token $ACCESS_TOKEN" https://api.github.com/repos/$repoOwner/$envProdRepo | jq 'contains({message: "Not Found"})'
+              curl -H "Authorization: token $ACCESS_TOKEN" https://api.github.com/repos/$repoOwner/$envStagingRepo | jq 'contains({message: "Not Found"})'
             ''', returnStdout: true)
           echo repoNotExists
           if(repoNotExists) {
           sh(script: """
-              curl -H "Authorization: token $ACCESS_TOKEN" --data '{"name":"${envProdRepo}"}' https://api.github.com/orgs/${repoOwner}/repos
+              curl -H "Authorization: token $ACCESS_TOKEN" --data '{"name":"${envStagingRepo}"}' https://api.github.com/orgs/${repoOwner}/repos
             """)
           }
           //curl -H "Authorization: token ACCESS_TOKEN" --data '{"name":""}' https://api.github.com/orgs/ORGANISATION_NAME/repos
         }
         writeFile file: "deploy.yml", text: deployYaml
-        sh("sed -i.bak 's#REPLACE_IMAGE_TAG#gcr.io/core-workshop/${repoName}:${BUILD_NUMBER}#' deploy.yml")
+        sh("sed -i.bak 's#REPLACE_IMAGE_TAG#gcr.io/core-workshop/helloworld-nodejs:${repoName}-${BUILD_NUMBER}#' deploy.yml")
         sh("sed -i.bak 's#REPLACE_SERVICE_NAME#${repoName}#' deploy.yml")
         withCredentials([usernamePassword(credentialsId: githubCredentialId, usernameVariable: 'USERNAME', passwordVariable: 'ACCESS_TOKEN')]) {
           sh """
@@ -33,14 +33,14 @@ def call(imageName, imageTag, githubCredentialId, repoOwner) {
             git config user.email "deployBot@cb-sa.io"
             git config user.name "${USERNAME}"
             git add deploy.yml
-            git commit -a -m 'updating ${envProdRepo} deployment for ${repoName}'
-            git remote add origin https://${USERNAME}:${ACCESS_TOKEN}@github.com/bee-cd/${envProdRepo}.git
+            git commit -a -m 'updating ${envStagingRepo} deployment for ${repoName}'
+            git remote add origin https://${USERNAME}:${ACCESS_TOKEN}@github.com/bee-cd/${envStagingRepo}.git
             git push -u origin master
           """
         }
         container("kubectl") {
           sh "kubectl apply -f deploy.yml"
-          sh "echo 'deployed to http://prod.cb-sa.io/${repoName}/'"
+          sh "echo 'deployed to http://staging.cb-sa.io/${repoName}/'"
         }
       }
     }
