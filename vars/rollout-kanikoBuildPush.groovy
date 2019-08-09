176f7da9298226a1b75313ebe51f5262ea0e328c
Grigory Avsyuk <gavsyuk@Grigorys-MacBook-Pro.local>
2019-08-09 17:19:36 -0400

    Create libraries for Rollout workshop

---
+++ vars/rollout-kanikoBuildPush.groovy
@@ -0,0 +1,23 @@
+// vars/kanikoBuildPush.groovy
+def call(String imageName, String imageTag = env.BUILD_NUMBER, String gcpProject = "core-workshop", String target = ".", String dockerFile="Dockerfile", Closure body) {
+  def dockerReg = "gcr.io/${gcpProject}"
+  //imageName = "helloworld-nodejs"
+  def label = "kaniko-${UUID.randomUUID().toString()}"
+  def podYaml = libraryResource 'podtemplates/dockerBuildPush.yml'
+  podTemplate(name: 'kaniko', label: label, yaml: podYaml, inheritFrom: 'default-jnlp', nodeSelector: 'type=agent') {
+    node(label) {
+      body()
+      rollout-imageNameTag()
+      gitShortCommit()
+      def repoName = env.IMAGE_REPO.toLowerCase()
+      container(name: 'kaniko', shell: '/busybox/sh') {
+        withEnv(['PATH+EXTRA=/busybox:/kaniko']) {
+          sh """#!/busybox/sh
+            /kaniko/executor -f ${pwd()}/${dockerFile} -c ${pwd()} --build-arg context=${repoName} --build-arg buildNumber=${BUILD_NUMBER} --build-arg shortCommit=${env.SHORT_COMMIT} --build-arg commitAuthor=${env.COMMIT_AUTHOR} -d ${dockerReg}/${imageName}:${repoName}-${BUILD_NUMBER}
+          """
+        }
+      }
+      publishEvent event:jsonEvent("{'eventType':'containerImagePush', 'image':'${dockerReg}/${imageName}:${repoName}-${BUILD_NUMBER}'}"), verbose: true
+    }
+  }
+}
---
+++ vars/rollout-kubeDeploy.groovy
@@ -0,0 +1,55 @@
+// vars/kubeDeploy.groovy
+def call(imageName, imageTag, githubCredentialId, repoOwner) {
+    def label = "kubectl"
+    def podYaml = libraryResource 'podtemplates/kubeDeploy.yml'
+    def deployYaml = libraryResource "k8s/${imageName}-basicDeploy.yml"
+    def repoName = env.IMAGE_REPO.toLowerCase()
+    def envStagingRepo = "environment_staging"
+    def pullMaster = true
+    
+    podTemplate(name: 'kubectl', label: label, yaml: podYaml) {
+      node(label) {
+        //create environment repo for prod if it doesn't already exist
+        echo githubCredentialId
+        withCredentials([usernamePassword(credentialsId: githubCredentialId, usernameVariable: 'USERNAME', passwordVariable: 'ACCESS_TOKEN')]) {
+          echo repoOwner
+          echo envStagingRepo
+          def repoNotExists = sh(script: '''
+              curl -H "Authorization: token $ACCESS_TOKEN" https://api.github.com/repos/$repoOwner/$envStagingRepo | jq 'contains({message: "Not Found"})'
+            ''', returnStdout: true)
+          echo repoNotExists
+          if(repoNotExists) {
+            sh(script: """
+                curl -H "Authorization: token $ACCESS_TOKEN" --data '{"name":"${envStagingRepo}"}' https://api.github.com/orgs/${repoOwner}/repos
+              """)
+             pullMaster = false 
+          }
+          //curl -H "Authorization: token ACCESS_TOKEN" --data '{"name":""}' https://api.github.com/orgs/ORGANISATION_NAME/repos
+        }
+        writeFile file: "deploy.yml", text: deployYaml
+        sh("sed -i.bak 's#REPLACE_IMAGE_TAG#gcr.io/core-workshop/${imageName}:${repoName}-${BUILD_NUMBER}#' deploy.yml")
+        sh("sed -i.bak 's#REPLACE_SERVICE_NAME#${repoName}#' deploy.yml")
+        withCredentials([usernamePassword(credentialsId: githubCredentialId, usernameVariable: 'USERNAME', passwordVariable: 'ACCESS_TOKEN')]) {
+          sh """
+            git init
+            git config user.email "deployBot@cb-sa.io"
+            git config user.name "${USERNAME}"
+            git remote add origin https://${USERNAME}:${ACCESS_TOKEN}@github.com/${repoOwner}/${envStagingRepo}.git
+          """
+          if(pullMaster) {
+            sh 'git pull origin master'
+          } else {
+            sh 'git add deploy.yml'
+          }
+          sh """
+            git commit -a -m 'updating ${envStagingRepo} deployment for ${repoName}'
+            git push -u origin master
+          """
+        }
+        container("kubectl") {
+          sh "kubectl apply -f deploy.yml"
+          sh "echo 'deployed to http://staging.cb-sa.io/${repoName}/'"
+        }
+      }
+    }
+}
