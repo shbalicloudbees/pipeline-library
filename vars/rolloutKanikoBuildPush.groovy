38ff793f67662df79a2ea78a1c0231982c82488f
Grigory Avsyuk <gavsyuk@Grigorys-MacBook-Pro.local>
2019-08-10 16:54:53 -0400

    Print test

--- vars/rolloutKanikoBuildPush.groovy
+++ vars/rolloutKanikoBuildPush.groovy
@@ -19,6 +19,7 @@ def call(String imageName, String imageTag = env.BUILD_NUMBER, String gcpProject
       def repoName = env.IMAGE_REPO.toLowerCase()
       container(name: 'kaniko', shell: '/busybox/sh') {
         withEnv(['PATH+EXTRA=/busybox:/kaniko']) {
+          sh "ls"
           echo "Dockerfile path: ${pwd()}/${folderName}/${dockerFile}"
           sh """#!/busybox/sh
             /kaniko/executor -f ${pwd()}/${folderName}/${dockerFile} -c ${pwd()} --build-arg context=${repoName} --build-arg buildNumber=${BUILD_NUMBER} --build-arg shortCommit=${env.SHORT_COMMIT} --build-arg commitAuthor=${env.COMMIT_AUTHOR} -d ${dockerReg}/${imageName}:${repoName}-${BUILD_NUMBER}
