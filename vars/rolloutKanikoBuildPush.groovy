41fc95ff7cf9f956653ca626619c730ae903ca33
Grigory Avsyuk <gavsyuk@Grigorys-MacBook-Pro.local>
2019-08-10 16:45:35 -0400

    Update path to dockerfile

--- vars/rolloutKanikoBuildPush.groovy
+++ vars/rolloutKanikoBuildPush.groovy
@@ -4,8 +4,15 @@ def call(String imageName, String imageTag = env.BUILD_NUMBER, String gcpProject
   //imageName = "helloworld-nodejs"
   def label = "kaniko-${UUID.randomUUID().toString()}"
   def podYaml = libraryResource 'podtemplates/dockerBuildPush.yml'
+  def folderName = ""
   podTemplate(name: 'kaniko', label: label, yaml: podYaml, inheritFrom: 'default-jnlp', nodeSelector: 'type=agent') {
     node(label) {
+      if(imageName=="rollout-js"){
+        folderName = "frontend-spring-boot-react-crud-full-stack-with-maven"
+      }
+      else if(imageName=="rollout-java"){
+        folderName = "backend-spring-boot-react-crud-full-stack-with-maven"
+      }
       body()
       rolloutImageNameTag()
       gitShortCommit()
@@ -13,7 +20,7 @@ def call(String imageName, String imageTag = env.BUILD_NUMBER, String gcpProject
       container(name: 'kaniko', shell: '/busybox/sh') {
         withEnv(['PATH+EXTRA=/busybox:/kaniko']) {
           sh """#!/busybox/sh
-            /kaniko/executor -f ${pwd()}/${dockerFile} -c ${pwd()} --build-arg context=${repoName} --build-arg buildNumber=${BUILD_NUMBER} --build-arg shortCommit=${env.SHORT_COMMIT} --build-arg commitAuthor=${env.COMMIT_AUTHOR} -d ${dockerReg}/${imageName}:${repoName}-${BUILD_NUMBER}
+            /kaniko/executor -f ${pwd()}/${folderName}/${dockerFile} -c ${pwd()} --build-arg context=${repoName} --build-arg buildNumber=${BUILD_NUMBER} --build-arg shortCommit=${env.SHORT_COMMIT} --build-arg commitAuthor=${env.COMMIT_AUTHOR} -d ${dockerReg}/${imageName}:${repoName}-${BUILD_NUMBER}
           """
         }
       }
