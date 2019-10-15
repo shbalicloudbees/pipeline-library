56f569028baacf5db5ce3eef35cb8b279e90c7ac
Kurt Madel <kmadel@cloudbees.com>
2019-10-15 15:48:00 -0400

    Update kanikoBuildPushGeneric.groovy

--- vars/kanikoBuildPushGeneric.groovy
+++ vars/kanikoBuildPushGeneric.groovy
@@ -9,7 +9,7 @@ def call(String imageName, String imageTag = env.BUILD_NUMBER, String gcpProject
       imageNameTag()
       gitShortCommit()
       container('gcp-sdk') {
-        sh 'docker-credential-gcr config --token-source="gcloud"'
+        sh 'gcloud auth configure-docker'
       }
       container(name: 'kaniko', shell: '/busybox/sh') {
         withEnv(['PATH+EXTRA=/busybox:/kaniko']) {
