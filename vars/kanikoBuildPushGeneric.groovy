1569b1aff50457df90ef508bfdf622112f7d3f09
Kurt Madel <kmadel@cloudbees.com>
2019-10-16 08:31:08 -0400

    Update kanikoBuildPushGeneric.groovy

--- vars/kanikoBuildPushGeneric.groovy
+++ vars/kanikoBuildPushGeneric.groovy
@@ -17,7 +17,7 @@ def call(String imageName, String imageTag = env.BUILD_NUMBER, String gcpProject
         }
       }
       container('gcp-sdk') {
-        sh 'gcloud docker --push ${dockerReg}/${imageName}:${imageTag}'
+        sh 'gcloud docker -- push ${dockerReg}/${imageName}:${imageTag}'
       }
     }
   }
