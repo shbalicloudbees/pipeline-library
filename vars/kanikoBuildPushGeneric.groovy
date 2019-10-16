f58c09cfc51787d8d95c355bd4cc38fc17f8dd60
Kurt Madel <kmadel@cloudbees.com>
2019-10-16 09:31:43 -0400

    Update kanikoBuildPushGeneric.groovy

--- vars/kanikoBuildPushGeneric.groovy
+++ vars/kanikoBuildPushGeneric.groovy
@@ -1,22 +1,20 @@
 // vars/kanikoBuildPush.groovy
 def call(String imageName, String imageTag = env.BUILD_NUMBER, String gcpProject = "core-workshop", String target = ".", String dockerFile="Dockerfile", Closure body) {
   def dockerReg = "gcr.io/${gcpProject}"
-  def label = "kaniko-${UUID.randomUUID().toString()}"
+  def label = "img-${UUID.randomUUID().toString()}"
   def podYaml = libraryResource 'podtemplates/dockerBuildPush.yml'
-  podTemplate(name: 'kaniko', label: label, yaml: podYaml, nodeSelector: 'type=agent') {
+  podTemplate(name: 'img', label: label, yaml: podYaml, nodeSelector: 'type=agent') {
     node(label) {
       body()
       imageNameTag()
       gitShortCommit()
       container('gcp-sdk') {
-        sh "gcloud alpha auth list"
+        sh "gcloud auth configure-docker"
       }
-      container(name: 'kaniko', shell: '/busybox/sh') {
-        withEnv(['PATH+EXTRA=/busybox:/kaniko']) {
-          sh """#!/busybox/sh
-            /kaniko/executor -f ${pwd()}/${dockerFile} -c ${pwd()} --build-arg buildNumber=${BUILD_NUMBER} --build-arg shortCommit=${env.SHORT_COMMIT} --build-arg commitAuthor=${env.COMMIT_AUTHOR} --no-push -d ${dockerReg}/${imageName}:${imageTag} 
-          """
-        }
+      container('img') {
+        sh """
+          img build -t ${dockerReg}/${imageName}:${imageTag} .  
+        """
       }
     }
   }
