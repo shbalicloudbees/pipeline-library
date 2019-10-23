9051a9452579a8e50dfd5763800939fdd2bcab33
Logan Donley <ldonley@cloudbees.com>
2019-10-23 10:54:26 -0400

    Adding namespace flag for anthos

--- vars/cloudRunDeploy.groovy
+++ vars/cloudRunDeploy.groovy
@@ -6,10 +6,10 @@ def call(Map config) {
     node(label) {
       container(name: 'gcp-sdk') {
         if (config.deployType == "gke") {
-          sh "gcloud beta run deploy ${config.serviceName} --image ${config.image} --allow-unauthenticated --platform gke --cluster ${config.clusterName} --cluster-location ${config.region}"
+          sh "gcloud beta run deploy ${config.serviceName} --image ${config.image} --allow-unauthenticated --platform gke --cluster ${config.clusterName} --cluster-location ${config.region} --namespace ${config.namespace}"
         }
         else if (config.deployType == "vmware") {
-          sh "gcloud beta run deploy ${config.serviceName} --image ${config.image} --allow-unauthenticated --platform kubernetes --kubeconfig ${config.kubeconfig}"
+          sh "gcloud beta run deploy ${config.serviceName} --image ${config.image} --allow-unauthenticated --platform kubernetes --namespace ${config.namespace} --kubeconfig ${config.kubeconfig}"
         }
         else {
           sh "gcloud beta run deploy ${config.serviceName} --image ${config.image} --allow-unauthenticated --region ${config.region} --platform managed"
