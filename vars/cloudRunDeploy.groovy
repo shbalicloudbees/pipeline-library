73f593413363ac997fd33b2b69b4b8cd966b535f
Logan Donley <ldonley@cloudbees.com>
2019-10-23 10:43:42 -0400

    Fixing issue

--- vars/cloudRunDeploy.groovy
+++ vars/cloudRunDeploy.groovy
@@ -2,22 +2,19 @@
 def call(Map config) {
   def podYaml = libraryResource 'podtemplates/cloud-run.yml'
   def label = "cloudrun-${UUID.randomUUID().toString()}"
-  println(config)
   podTemplate(name: 'cloud-run-pod', label: label, yaml: podYaml, nodeSelector: 'type=agent') {
     node(label) {
       container(name: 'gcp-sdk') {
         if (config.deployType == "gke") {
-          sh "gcloud beta run deploy ${config.serviceName} --image ${config.image} --allow-unauthenticated --platform gke --cluster ${config.clusterName} --cluster-location ${config.gcpRegion}"
+          sh "gcloud beta run deploy ${config.serviceName} --image ${config.image} --allow-unauthenticated --platform gke --cluster ${config.clusterName} --cluster-location ${config.region}"
         }
         else if (config.deployType == "vmware") {
           sh "gcloud beta run deploy ${config.serviceName} --image ${config.image} --allow-unauthenticated --platform kubernetes --kubeconfig ${config.kubeconfig}"
         }
         else {
-          sh "gcloud beta run deploy ${config.serviceName} --image ${config.image} --allow-unauthenticated --platform managed --region ${config.gcpRegion}"
+          sh "gcloud beta run deploy ${config.serviceName} --image ${config.image} --allow-unauthenticated --region ${config.region} --platform managed"
         } 
       }
     }
   }
 }
-
-// String serviceName, String image, String deployType = "managed", String gcpRegion = "us-central1", String kubeconfig = "", String clusterName = ""
