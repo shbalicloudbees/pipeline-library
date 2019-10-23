102c93f0379a772c198c22822d23c7d3400a4ea9
Logan Donley <ldonley@cloudbees.com>
2019-10-23 10:36:43 -0400

    Switching cloudRunDeploy to use map

--- vars/cloudRunDeploy.groovy
+++ vars/cloudRunDeploy.groovy
@@ -1,20 +1,22 @@
 // vars/cloudRunDeploy.groovy
-def call(String serviceName, String image, String deployType = "managed", String gcpRegion = "us-central1", String kubeconfig = "", String clusterName = "") {
+def call(Map config) {
   def podYaml = libraryResource 'podtemplates/cloud-run.yml'
   def label = "cloudrun-${UUID.randomUUID().toString()}"
   podTemplate(name: 'cloud-run-pod', label: label, yaml: podYaml, nodeSelector: 'type=agent') {
     node(label) {
       container(name: 'gcp-sdk') {
-        if (deployType == "gke") {
-          sh "gcloud beta run deploy ${serviceName} --image ${image} --allow-unauthenticated --platform gke --cluster ${clusterName} --cluster-location ${gcpRegion}"
+        if (config.deployType == "gke") {
+          sh "gcloud beta run deploy ${config.serviceName} --image ${config.image} --allow-unauthenticated --platform gke --cluster ${config.clusterName} --cluster-location ${config.gcpRegion}"
         }
-        else if (deployType == "vmware") {
-          sh "gcloud beta run deploy ${serviceName} --image ${image} --allow-unauthenticated --platform kubernetes --kubeconfig ${kubeconfig}"
+        else if (config.deployType == "vmware") {
+          sh "gcloud beta run deploy ${config.serviceName} --image ${config.image} --allow-unauthenticated --platform kubernetes --kubeconfig ${config.kubeconfig}"
         }
         else {
-          sh "gcloud beta run deploy ${serviceName} --image ${image} --allow-unauthenticated --platform managed --region ${gcpRegion}"
+          sh "gcloud beta run deploy ${config.serviceName} --image ${config.image} --allow-unauthenticated --platform managed --region ${config.gcpRegion}"
         } 
       }
     }
   }
 }
+
+// String serviceName, String image, String deployType = "managed", String gcpRegion = "us-central1", String kubeconfig = "", String clusterName = ""
