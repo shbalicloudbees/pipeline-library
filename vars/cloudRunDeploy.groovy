b0ad715e88ca3056e7437499ed9811f1f1b4df17
Logan Donley <ldonley@cloudbees.com>
2019-10-23 10:24:33 -0400

    Fixing cloudRunDeploy

--- vars/cloudRunDeploy.groovy
+++ vars/cloudRunDeploy.groovy
@@ -1,7 +1,8 @@
 // vars/cloudRunDeploy.groovy
 def call(String serviceName, String image, String deployType = "managed", String gcpRegion = "us-central1", String kubeconfig = "", String clusterName = "") {
   def podYaml = libraryResource 'podtemplates/cloud-run.yml'
-  podTemplate(name: 'kaniko', label: label, yaml: podYaml, nodeSelector: 'type=agent') {
+  def label = "cloudrun-${UUID.randomUUID().toString()}"
+  podTemplate(name: 'cloud-run-pod', label: label, yaml: podYaml, nodeSelector: 'type=agent') {
     node(label) {
       container(name: 'gcp-sdk') {
         if (deployType == "gke") {
