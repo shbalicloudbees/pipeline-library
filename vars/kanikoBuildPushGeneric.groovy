afe6928aec6d918bc706d737251080768aeb5a9c
Kurt Madel <kmadel@cloudbees.com>
2019-10-26 06:58:42 -0400

    Update kanikoBuildPushGeneric.groovy

--- vars/kanikoBuildPushGeneric.groovy
+++ vars/kanikoBuildPushGeneric.groovy
@@ -12,7 +12,7 @@ def call(String imageName, String imageTag = env.BUILD_NUMBER, String gcpProject
         sh """
           img build --build-arg buildNumber=${BUILD_NUMBER} --build-arg shortCommit=${env.SHORT_COMMIT} --build-arg commitAuthor="${env.COMMIT_AUTHOR}" -t ${dockerReg}/${imageName}:${imageTag} ${pwd()}
           /home/user/google-cloud-sdk/bin/gcloud auth print-access-token | img login -u oauth2accesstoken --password-stdin https://gcr.io
-          img ${dockerReg}/${imageName}:${imageTag}
+          img push ${dockerReg}/${imageName}:${imageTag}
         """
       }
     }
