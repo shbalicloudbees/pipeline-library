20e92106f1cabe038fdceaeb08d057f65ce6742a
Kurt Madel <kmadel@cloudbees.com>
2019-11-11 07:38:02 -0500

    Update cloudRunDeploy.groovy

--- vars/cloudRunDeploy.groovy
+++ vars/cloudRunDeploy.groovy
@@ -20,6 +20,7 @@ def call(Map config) {
       sh "cat run.json"
       CLOUD_RUN_URL = sh (script: "cat run.json | jq -r '.url' | tr -d '\n'", 
                 returnStdout: true)
+      echo env.CHANGE_ID
       withCredentials([usernamePassword(credentialsId: "${githubCredentialId}", usernameVariable: 'USERNAME', passwordVariable: 'TOKEN')]) {
         sh """
           curl -s -H "Authorization: token ${TOKEN}" \
