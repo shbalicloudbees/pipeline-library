c2f6a69b7daa638ced3daee38fdf17cc70a4a0a4
Kurt Madel <kmadel@cloudbees.com>
2019-11-11 05:25:24 -0500

    Update cloudRunDeploy.groovy

--- vars/cloudRunDeploy.groovy
+++ vars/cloudRunDeploy.groovy
@@ -2,6 +2,7 @@
 def call(Map config) {
   def podYaml = libraryResource 'podtemplates/cloud-run.yml'
   def label = "cloudrun-${UUID.randomUUID().toString()}"
+  def CLOUD_RUN_URL
   podTemplate(name: 'cloud-run-pod', label: label, yaml: podYaml, nodeSelector: 'workload=general') {
     node(label) {
       container(name: 'gcp-sdk') {
@@ -13,8 +14,16 @@ def call(Map config) {
         }
         else {
           sh "gcloud beta run deploy ${config.serviceName} --image ${config.image} --allow-unauthenticated --region ${config.region} --platform managed --format=json"
-          sh "gcloud beta run services describe ${config.serviceName} --region ${config.region} --platform managed --format=json"
+          CLOUD_RUN_URL = sh (script: "gcloud beta run services describe ${config.serviceName} --region ${config.region} --platform managed --format=json | jq -r '.url' | tr -d '\n'", 
+                returnStdout: true)
         } 
+      }
+      withCredentials([usernamePassword(credentialsId: 'cbdays-github-token', usernameVariable: 'USERNAME', passwordVariable: 'TOKEN')]) {
+        sh """
+          curl -s -H "Authorization: token ${TOKEN}" \
+            -X POST -d '{"body": "Preview Environment URL: ${CLOUD_RUN_URL}"}' \
+            "https://api.github.com/repos/cloudbees-days/blog/issues/10/comments"
+        """
       }
     }
   }
