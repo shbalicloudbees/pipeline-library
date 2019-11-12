f095aec5b3ba02f1694dbde1026648a582395587
Kurt Madel <kmadel@cloudbees.com>
2019-11-12 06:39:24 -0500

    Update cloudRunDeploy.groovy

--- vars/cloudRunDeploy.groovy
+++ vars/cloudRunDeploy.groovy
@@ -7,25 +7,29 @@ def call(Map config) {
     node(label) {
       container(name: 'gcp-sdk') {
         if (config.deployType == "gke") {
-          sh "gcloud beta run deploy ${config.serviceName} --image ${config.image} --platform gke --cluster ${config.clusterName} --cluster-location ${config.region} --namespace ${config.namespace} --format=json"
+          sh "gcloud beta run deploy ${config.serviceName} --image ${config.image} --platform gke --cluster ${config.clusterName} --cluster-location ${config.region} --namespace ${config.namespace}"
+          sh "gcloud beta run services describe ${config.serviceName} --platform gke --cluster ${config.clusterName} --cluster-location ${config.region} --namespace ${config.namespace} --format=json > run.json 2>&1"
         }
         else if (config.deployType == "vmware") {
-          sh "gcloud beta run deploy ${config.serviceName} --image ${config.image} --platform kubernetes --namespace ${config.namespace} --kubeconfig ${config.kubeconfig} --format=json"
+          sh "gcloud beta run deploy ${config.serviceName} --image ${config.image} --platform kubernetes --namespace ${config.namespace} --kubeconfig ${config.kubeconfig}"
+          sh "gcloud beta run services describe ${config.serviceName} --platform kubernetes --kubeconfig ${config.kubeconfig} --format=json > run.json 2>&1"
         }
         else {
-          sh "gcloud beta run deploy ${config.serviceName} --image ${config.image} --allow-unauthenticated --region ${config.region} --platform managed --format=json"
+          sh "gcloud beta run deploy ${config.serviceName} --image ${config.image} --allow-unauthenticated --region ${config.region} --platform managed"
           sh "gcloud beta run services describe ${config.serviceName} --region ${config.region} --platform managed --format=json > run.json 2>&1"
         } 
       }
       sh "cat run.json"
-      CLOUD_RUN_URL = sh (script: "cat run.json | jq -r '.status.url' | tr -d '\n'", 
-                returnStdout: true)
-      withCredentials([usernamePassword(credentialsId: "${credId}", usernameVariable: 'USERNAME', passwordVariable: 'TOKEN')]) {
-        sh """
-          curl -s -H "Authorization: token ${TOKEN}" \
-            -X POST -d '{"body": "Preview Environment URL: ${CLOUD_RUN_URL}"}' \
-            "https://api.github.com/repos/${repoOwner}/${repo}/issues/${env.CHANGE_ID}/comments"
-        """
+      if (env.CHANGE_ID) {
+        CLOUD_RUN_URL = sh (script: "cat run.json | jq -r '.status.url' | tr -d '\n'", 
+                  returnStdout: true)
+        withCredentials([usernamePassword(credentialsId: "${credId}", usernameVariable: 'USERNAME', passwordVariable: 'TOKEN')]) {
+          sh """
+            curl -s -H "Authorization: token ${TOKEN}" \
+              -X POST -d '{"body": "Preview Environment URL: ${CLOUD_RUN_URL}"}' \
+              "https://api.github.com/repos/${repoOwner}/${repo}/issues/${env.CHANGE_ID}/comments"
+          """
+        }
       }
     }
   }
