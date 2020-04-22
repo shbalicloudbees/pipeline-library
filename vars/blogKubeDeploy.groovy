a4095a1b71ddfecbfc3cc3c36b15e6347e35503e
Beedemo Developer <36600505+beedemo-dev@users.noreply.github.com>
2020-04-22 17:08:27 -0400

    Update blogKubeDeploy.groovy

--- vars/blogKubeDeploy.groovy
+++ vars/blogKubeDeploy.groovy
@@ -1,32 +1,21 @@
 // vars/kubeDeploy.groovy
-def call(imageName, gcpProject, githubCredentialId, repoOwner) {
+def call(repoName, dockerRegistryDomain, deploymentDomain, repoOwner, gcpProject = "core-workshop") {
     def label = "kubectl"
     def podYaml = libraryResource 'podtemplates/kubeDeploy.yml'
     def deployYaml = libraryResource 'k8s/basicDeploy.yml'
-    def envStagingRepo = "environment_staging"
     
     podTemplate(name: 'kubectl', label: label, yaml: podYaml) {
       node(label) {
         imageNameTag()
-        def repoName = env.IMAGE_REPO.toLowerCase()
-        //create environment repo for prod if it doesn't already exist
-        echo githubCredentialId
-        withCredentials([usernamePassword(credentialsId: githubCredentialId, usernameVariable: 'USERNAME', passwordVariable: 'ACCESS_TOKEN')]) {
-          
-          // try {
-          //   sh 'git pull origin master'
-          // } catch(e) {
-          //   //nothing to do, just means remote hasn't been initialized yet
-          // }
-          checkout scm
-          sh("sed -i.bak 's#REPLACE_IMAGE#gcr.io/${gcpProject}/blog-vuejs:${repoName}-${BUILD_NUMBER}#' .kubernetes/frontend.yaml")
-          sh("sed -i.bak 's#REPLACE_HOSTNAME#preview.workshop.cb-sa.io#' .kubernetes/frontend.yaml")
-          sh("sed -i.bak 's#REPLACE_PATH#/${repoOwner}#' .kubernetes/frontend.yaml")
-        }
+        def imageName = repoName.toLowerCase()
+        checkout scm
+        sh("sed -i.bak 's#REPLACE_IMAGE#${dockerRegistryDomain}/${imageName}:${env.VERSION}#' .kubernetes/frontend.yaml")
+        sh("sed -i.bak 's#REPLACE_HOSTNAME#staging.${repoOwner}.${deploymentDomain}#' .kubernetes/frontend.yaml")
+        sh("sed -i.bak 's#REPLACE_PATH#/${repoOwner}#' .kubernetes/frontend.yaml")
         container("kubectl") {
           sh "cat .kubernetes/frontend.yaml"
           sh "kubectl apply -f .kubernetes/frontend.yaml"
-          sh "echo 'deployed to http://preview.workshop.cb-sa.io/${repoName}/'"
+          sh "echo 'deployed to http://staging.${repoOwner}.${deploymentDomain}'"
         }
       }
     }
