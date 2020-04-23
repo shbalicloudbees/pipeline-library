bc1808cdc0128c3cd14ecf6f0f09ea4a87e0ef25
Beedemo Developer <36600505+beedemo-dev@users.noreply.github.com>
2020-04-23 10:33:52 -0400

    Update imgBuildNexus.groovy

--- vars/imgBuildNexus.groovy
+++ vars/imgBuildNexus.groovy
@@ -11,7 +11,6 @@ def call(String repoName, String repoOwner, String registry, Closure body) {
         env.VERSION = env.VERSION.trim()
       }
       imageNameTag()
-      gitShortCommit()
       container('img') {
         sh """
           img build --build-arg buildNumber=${BUILD_NUMBER} --build-arg shortCommit=${env.SHORT_COMMIT} --build-arg commitAuthor="${env.COMMIT_AUTHOR}" -t ${registry}/${repoOwner}/${imageName}:${env.VERSION} ${pwd()}
