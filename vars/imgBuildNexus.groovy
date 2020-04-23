e15a9d05c6f4d8596ebd0f1040b5e81cba179d3c
Beedemo Developer <36600505+beedemo-dev@users.noreply.github.com>
2020-04-23 10:35:32 -0400

    Update imgBuildNexus.groovy

--- vars/imgBuildNexus.groovy
+++ vars/imgBuildNexus.groovy
@@ -2,7 +2,7 @@
 def call(String repoName, String repoOwner, String registry, Closure body) {
   def label = "img-${UUID.randomUUID().toString()}"
   def podYaml = libraryResource 'podtemplates/imageBuildPushNexus.yml'
-  def imageName = repoName.toLowerCase()
+  def imageName = repoOwner.toLowerCase() + "/" + repoName.toLowerCase()
   podTemplate(name: 'img', label: label, yaml: podYaml) {
     node(label) {
       body()
@@ -13,8 +13,8 @@ def call(String repoName, String repoOwner, String registry, Closure body) {
       imageNameTag()
       container('img') {
         sh """
-          img build --build-arg buildNumber=${BUILD_NUMBER} --build-arg shortCommit=${env.SHORT_COMMIT} --build-arg commitAuthor="${env.COMMIT_AUTHOR}" -t ${registry}/${repoOwner}/${imageName}:${env.VERSION} ${pwd()}
-          img push ${registry}/${repoOwner}/${imageName}:${env.VERSION}
+          img build --build-arg buildNumber=${BUILD_NUMBER} --build-arg shortCommit=${env.SHORT_COMMIT} --build-arg commitAuthor="${env.COMMIT_AUTHOR}" -t ${registry}/${imageName}:${env.VERSION} ${pwd()}
+          img push ${registry}/${imageName}:${env.VERSION}
         """
       }
     }
