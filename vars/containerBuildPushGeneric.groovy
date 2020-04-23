d133955e224dac23c9c32bb2a77d223df8786fdb
Beedemo Developer <36600505+beedemo-dev@users.noreply.github.com>
2020-04-23 08:11:54 -0400

    Update containerBuildPushGeneric.groovy

--- vars/containerBuildPushGeneric.groovy
+++ vars/containerBuildPushGeneric.groovy
@@ -16,6 +16,7 @@ def call(String imageName, String imageTag = env.BUILD_NUMBER, String gcpProject
       container('img-gcloud') {
         sh """
           img build --build-arg buildNumber=${BUILD_NUMBER} --build-arg shortCommit=${env.SHORT_COMMIT} --build-arg commitAuthor="${env.COMMIT_AUTHOR}" -t ${dockerReg}/${imageName}:${imageTag} ${pwd()}
+          gcloud  auth configure-docker --quiet
           img push ${dockerReg}/${imageName}:${imageTag}
         """
       }
