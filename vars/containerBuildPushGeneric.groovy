be118cdfefdb00a077ad4c7460d05f3558db6d92
Beedemo Developer <36600505+beedemo-dev@users.noreply.github.com>
2020-04-30 14:10:02 -0400

    Update containerBuildPushGeneric.groovy

--- vars/containerBuildPushGeneric.groovy
+++ vars/containerBuildPushGeneric.groovy
@@ -4,6 +4,7 @@ def call(String imageName, String imageTag = env.BUILD_NUMBER, String gcpProject
   def label = "img-gcloud-${UUID.randomUUID().toString()}"
   def podYaml = libraryResource 'podtemplates/containerBuildPush.yml'
   def customBuildArg = ""
+  def buildModeArg = ""
   podTemplate(name: 'img-gcloud', label: label, yaml: podYaml) {
     node(label) {
       body()
@@ -16,10 +17,13 @@ def call(String imageName, String imageTag = env.BUILD_NUMBER, String gcpProject
       if(env.EVENT_PUSH_IMAGE_TAG) {
         customBuildArg = "--build-arg NODE_IMAGE=${env.EVENT_PUSH_IMAGE_NAME}:${env.EVENT_PUSH_IMAGE_TAG}"
       }
+      if($BRANCH_NAME != "master") {
+        buildModeArg = "--build-arg BUILD_MODE=build:dev" 
+      }
       imageName = imageName.toLowerCase()
       container('img-gcloud') {
         sh """
-          img build --build-arg buildNumber=${BUILD_NUMBER} ${customBuildArg} --build-arg shortCommit=${env.SHORT_COMMIT} --build-arg commitAuthor="${env.COMMIT_AUTHOR}" -t ${dockerReg}/${imageName}:${imageTag} ${pwd()}
+          img build --build-arg buildNumber=${BUILD_NUMBER} ${customBuildArg} ${customBuildArg} --build-arg shortCommit=${env.SHORT_COMMIT} --build-arg commitAuthor="${env.COMMIT_AUTHOR}" -t ${dockerReg}/${imageName}:${imageTag} ${pwd()}
           gcloud  auth configure-docker --quiet
           img push ${dockerReg}/${imageName}:${imageTag}
         """
