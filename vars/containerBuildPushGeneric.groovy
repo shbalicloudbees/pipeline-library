48721b6fabeff3eb08b579152135c917e8988814
Beedemo Developer <36600505+beedemo-dev@users.noreply.github.com>
2020-05-03 12:39:47 -0400

    Update containerBuildPushGeneric.groovy

--- vars/containerBuildPushGeneric.groovy
+++ vars/containerBuildPushGeneric.groovy
@@ -23,6 +23,7 @@ def call(String imageName, String imageTag = env.BUILD_NUMBER, String gcpProject
       imageName = imageName.toLowerCase()
       container('gcp-sdk') {
         try {
+          sh "printevn"
           sh "echo $prevImageTag"
           sh "gcloud container images delete ${dockerReg}/${imageName}:${prevImageTag}  --force-delete-tags --quiet"
         } catch(e) {}
@@ -31,9 +32,9 @@ def call(String imageName, String imageTag = env.BUILD_NUMBER, String gcpProject
         sh """
           img build ${buildModeArg} --build-arg buildNumber=${BUILD_NUMBER} ${customBuildArg} ${customBuildArg} --build-arg shortCommit=${env.SHORT_COMMIT} --build-arg commitAuthor="${env.COMMIT_AUTHOR}" -t ${dockerReg}/${imageName}:${imageTag} ${pwd()}
           cat /home/user/key/gcr-key.json | img login -u _json_key --password-stdin https://gcr.io
-          img push ${dockerReg}/${imageName}:${imageTag}
-          export prevImageTag=${imageTag}
+          img push ${dockerReg}/${imageName}:${imageTag}     
         """
+        env.prevImageTag=${imageTag}
       }
     }
   }
