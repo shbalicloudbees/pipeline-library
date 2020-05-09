fd4b4323115b7ce7b443a9316e463294b13ca3c4
Beedemo Developer <36600505+beedemo-dev@users.noreply.github.com>
2020-05-09 12:08:18 -0400

    Create gitHubCommitStatus.groovy

---
+++ vars/gitHubCommitStatus.groovy
@@ -0,0 +1,9 @@
+def call(repoName, repoOwner, sha, targetUrl, description, context, state="success") {        
+  withCredentials([usernamePassword(credentialsId: "${githubCredentialId}", usernameVariable: 'USERNAME', passwordVariable: 'TOKEN')]) {
+    sh """
+      curl -s -H "Authorization: token ${TOKEN}" \
+        -X POST -d '{"state": "${state}", "target_url": "${targetUrl}", "description": "${description}", "context": "cloudbees-ci/${context}"}' \
+        "https://api.github.com/repos/${repoOwner}/${repo}/statuses/${sha}"
+    """
+  }
+} 
