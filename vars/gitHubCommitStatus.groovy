40bd9e153c0ac717865a30e63b0c485223661cb0
Beedemo Developer <36600505+beedemo-dev@users.noreply.github.com>
2020-05-09 12:29:19 -0400

    Update gitHubCommitStatus.groovy

--- vars/gitHubCommitStatus.groovy
+++ vars/gitHubCommitStatus.groovy
@@ -3,7 +3,7 @@ def call(repoName, repoOwner, sha, targetUrl, description, context, state="succe
     sh """
       curl -s -H "Authorization: token ${TOKEN}" \
         -X POST -d '{"state": "${state}", "target_url": "${targetUrl}", "description": "${description}", "context": "cloudbees-ci/${context}"}' \
-        "https://api.github.com/repos/${repoOwner}/${repo}/statuses/${sha}"
+        "https://api.github.com/repos/${repoOwner}/${repoName}/statuses/${sha}"
     """
   }
 } 
