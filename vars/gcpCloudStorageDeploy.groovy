d7e4131b4e64f0b3863fe471a510c69cbc5e7897
Kurt Madel <kmadel@cloudbees.com>
2020-08-20 18:02:31 -0400

    Update gcpCloudStorageDeploy.groovy

--- vars/gcpCloudStorageDeploy.groovy
+++ vars/gcpCloudStorageDeploy.groovy
@@ -1,10 +1,11 @@
 // vars/gcpCloudStorageDeploy.groovy
-def call(Map config) {
+def call(Map config, Closure body) {
   def podYaml = libraryResource 'podtemplates/google-cloud-sdk.yml'
   def label = "cloud-sdk-${UUID.randomUUID().toString()}"
   def CLOUD_RUN_URL
   podTemplate(name: 'cloud-sdk', label: label, yaml: podYaml) {
     node(label) {
+      body()
       container(name: 'gcp-sdk') {
         sh "gsutil -m cp -r public/** gs://${config.bucket}"
     }
