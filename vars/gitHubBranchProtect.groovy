3efe74ba3dce37fc70e992060486f76fc38e700f
Kurt Madel <kmadel@cloudbees.com>
2020-08-30 14:20:05 -0400

    Create gitHubBranchProtect.groovy

---
+++ vars/gitHubBranchProtect.groovy
@@ -0,0 +1,12 @@
+def call(String gitHubOrg, String gitHubRepo, String credentialId = 'cloudbees-ci-workshop-github-app') {        
+  withCredentials([usernamePassword(credentialsId: "${credentialId}", usernameVariable: 'GITHUB_APP', passwordVariable: 'GITHUB_ACCESS_TOKEN')]) {
+    sh """
+      curl \
+        -X PUT \
+        -H 'authorization: Bearer ${GITHUB_ACCESS_TOKEN}' \
+        -H 'Accept: application/vnd.github.antiope-preview+json' \
+        "https://api.github.com/repos/${gitHubOrg}/${gitHubRepo}/branches/master/protection" \
+        --data '{"required_status_checks":{"strict":false,"contexts":["error","pmd","checkstyle"]},"enforce_admins":,"required_pull_request_reviews":,"restrictions":}'
+    """
+  }
+} 
