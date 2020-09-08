146ef81fa5483b911c084fcc24409229712edeeb
Kurt Madel <kmadel@cloudbees.com>
2020-09-08 11:46:28 -0400

    Update gitHubComment.groovy

--- vars/gitHubComment.groovy
+++ vars/gitHubComment.groovy
@@ -1,7 +1,7 @@
 def call(Map config) {        
-  withCredentials([usernamePassword(credentialsId: "${config.credId}", usernameVariable: 'USERNAME', passwordVariable: 'TOKEN')]) {
+  withCredentials([usernamePassword(credentialsId: "${config.credId}", usernameVariable: 'GITHUB_APP', passwordVariable: 'GITHUB_ACCESS_TOKEN')]) {
     sh """
-      curl -s -H "Authorization: token ${TOKEN}" \
+      curl -s -H "Authorization: Bearer ${GITHUB_ACCESS_TOKEN}" \
         -X POST -d '{"body": "${config.message}"}' \
         "https://api.github.com/repos/${config.repoOwner}/${config.repo}/issues/${config.issueId}/comments"
     """
