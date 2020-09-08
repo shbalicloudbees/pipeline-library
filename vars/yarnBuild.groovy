be1bc33a4b06a9896a4598c4fd2234ad23c96734
Kurt Madel <kmadel@cloudbees.com>
2020-09-08 08:46:15 -0400

    Update yarnBuild.groovy

--- vars/yarnBuild.groovy
+++ vars/yarnBuild.groovy
@@ -1,18 +1,21 @@
-def call() {
+def call(String buildMode = "build", Closure body) {
   def label = "nodejs-${repoOwner}"
   def podYaml = libraryResource 'podtemplates/nodejs/pod.yml'
-  def buildMode = "build"
   podTemplate(name: 'nodejs', label: label, yaml: podYaml, podRetention: always(), idleMinutes: 30) {
-    body()
-    if(env.BRANCH_NAME != "master") {
-      buildMode = "build:dev" 
+    node(label) {
+      body()
+      if(env.BRANCH_NAME != "master") {
+        buildMode = "build:dev" 
+      }
+      container('nodejs') {
+        sh """
+          mkdir -p app
+          cd ./app
+          yarn install
+          yarn run $buildMode
+        """
+      }
+      stash name: "app", includes: "app/**" 
     }
-    sh """
-      mkdir -p app
-      cd ./app
-      yarn install
-      yarn run $buildMode
-    """
-    stash name: "app", includes: "app/**" 
   }
 }
