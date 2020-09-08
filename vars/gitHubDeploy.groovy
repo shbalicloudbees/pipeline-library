dfa033ee7dcdadfe939628b1ebf46d15db3aad61
Kurt Madel <kmadel@cloudbees.com>
2020-09-08 16:48:36 -0400

    Update gitHubDeploy.groovy

--- vars/gitHubDeploy.groovy
+++ vars/gitHubDeploy.groovy
@@ -7,6 +7,7 @@ def call(String gitHubOrg, String gitHubRepo, String deployUrl = "", String envi
         -H 'Accept: application/vnd.github.antiope-preview+json' \
         -H 'Accept: application/vnd.github.v3+json' \
         -H 'Accept: application/vnd.github.flash-preview+json' \
+        -H 'Accept: application/vnd.github.ant-man-preview+json' \
         https://api.github.com/repos/${gitHubOrg}/${gitHubRepo}/deployments \
         --data '{"ref":"${env.COMMIT_SHA}","environment":"${environment}","required_contexts":[],"description":"CloudBees CI Deployment","transient_environment":${transient}}' \
         | jq -r '.id' | tr -d '\n' 
