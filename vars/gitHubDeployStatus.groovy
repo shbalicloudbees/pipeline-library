26660a10ea8ed848dd94e2f07b20f0e9dc5f74ab
Kurt Madel <kmadel@cloudbees.com>
2020-09-08 17:34:13 -0400

    Update gitHubDeployStatus.groovy

--- vars/gitHubDeployStatus.groovy
+++ vars/gitHubDeployStatus.groovy
@@ -8,7 +8,7 @@ def call(String gitHubOrg, String gitHubRepo, String deployUrl, String status = 
         -H "Accept: application/vnd.github.v3+json" \
         -H "Accept: application/vnd.github.ant-man-preview+json"  \
         -H "Accept: application/vnd.github.flash-preview+json" \
-        https://api.github.com/repos/${gitHubOrg}/${gitHubRepo}/deployments/${GITHUB_DEPLOYMENT_ID}/statuses \
+        https://api.github.com/repos/${gitHubOrg}/${gitHubRepo}/deployments/${env.GITHUB_DEPLOYMENT_ID}/statuses \
         --data '{"state":"${status}","environment_url":"${deployUrl}","log_url":"${BUILD_URL}"}'
     """)
    
