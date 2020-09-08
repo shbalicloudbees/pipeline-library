86e94ba0c47ea157c6e5ead6b7d8dab5fb8d7bbe
Kurt Madel <kmadel@cloudbees.com>
2020-09-08 14:31:48 -0400

    Update gitHubDeploy.groovy

--- vars/gitHubDeploy.groovy
+++ vars/gitHubDeploy.groovy
@@ -7,7 +7,7 @@ def call(String gitHubOrg, String gitHubRepo, String deployUrl, String environme
         -H 'Accept: application/vnd.github.antiope-preview+json' \
         -H 'Accept: application/vnd.github.v3+json' \
         https://api.github.com/repos/${gitHubOrg}/${gitHubRepo}/deployments \
-        --data '{"ref":"${BRANCH_NAME}","environment":"${environment}","required_contexts":[],"description":"CloudBees CI Deployment"}' \
+        --data '{"ref":"${env.COMMIT_SHA}","environment":"${environment}","required_contexts":[],"description":"CloudBees CI Deployment"}' \
         | jq -r '.id' | tr -d '\n' 
     """, returnStdout: true)
     env.GITHUB_DEPLOYMENT_ID = deploymentId
