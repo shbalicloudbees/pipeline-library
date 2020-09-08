6f3c4b6b9b81ff74e7b6bb4b6abef22ff9d18829
Kurt Madel <kmadel@cloudbees.com>
2020-09-08 18:34:55 -0400

    Update gitHubDeploy.groovy

--- vars/gitHubDeploy.groovy
+++ vars/gitHubDeploy.groovy
@@ -2,7 +2,7 @@ def call(String gitHubOrg, String gitHubRepo, String deployUrl = "", String envi
   withCredentials([usernamePassword(credentialsId: "${credentialId}", usernameVariable: 'GITHUB_APP', passwordVariable: 'GITHUB_ACCESS_TOKEN')]) {
    def ref = env.CHANGE_BRANCH ?: env.BRANCH_NAME 
    def deploymentId =  sh(script: """
-      json = \$(curl \
+      curl \
         -X POST \
         -H 'authorization: Bearer ${GITHUB_ACCESS_TOKEN}' \
         -H 'Accept: application/vnd.github.antiope-preview+json' \
@@ -10,9 +10,8 @@ def call(String gitHubOrg, String gitHubRepo, String deployUrl = "", String envi
         -H 'Accept: application/vnd.github.flash-preview+json' \
         -H 'Accept: application/vnd.github.ant-man-preview+json' \
         https://api.github.com/repos/${gitHubOrg}/${gitHubRepo}/deployments \
-        --data '{"ref":"${ref}","environment":"${environment}","required_contexts":[],"description":"CloudBees CI Deployment","transient_environment":${transientEnv},"production_environment":${productionEnv}}')
-        
-      \$json | jq -r '.id' | tr -d '\n' 
+        --data '{"ref":"${ref}","environment":"${environment}","required_contexts":[],"description":"CloudBees CI Deployment","transient_environment":${transientEnv},"production_environment":${productionEnv}}' \
+      | jq -r '.id' | tr -d '\n' 
     """, returnStdout: true)
     env.GITHUB_DEPLOYMENT_ID = deploymentId
     gitHubDeployStatus(gitHubOrg, gitHubRepo, deployUrl)
