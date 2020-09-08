810ffad6e7e97dae47700ed95f542f662ab5bd7a
Kurt Madel <kmadel@cloudbees.com>
2020-09-08 10:53:55 -0400

    Update cloudRunDeploy.groovy

--- vars/cloudRunDeploy.groovy
+++ vars/cloudRunDeploy.groovy
@@ -7,16 +7,16 @@ def call(Map config) {
     node(label) {
       container(name: 'gcp-sdk') {
         if (config.deployType == "gke") {
-          sh "gcloud beta run deploy ${config.serviceName} --image ${config.image} --platform gke --cluster ${config.clusterName} --cluster-location ${config.region} --namespace ${config.namespace}"
-          sh "gcloud beta run services describe ${config.serviceName} --platform gke --cluster ${config.clusterName} --cluster-location ${config.region} --namespace ${config.namespace} --format=json > run.json 2>&1"
+          sh "gcloud run deploy ${config.serviceName} --image ${config.image} --platform gke --cluster ${config.clusterName} --cluster-location ${config.region} --namespace ${config.namespace}"
+          sh "gcloud run services describe ${config.serviceName} --platform gke --cluster ${config.clusterName} --cluster-location ${config.region} --namespace ${config.namespace} --format=json > run.json 2>&1"
         }
         else if (config.deployType == "vmware") {
-          sh "gcloud beta run deploy ${config.serviceName} --image ${config.image} --platform kubernetes --namespace ${config.namespace} --kubeconfig ${config.kubeconfig}"
-          sh "gcloud beta run services describe ${config.serviceName} --platform kubernetes --kubeconfig ${config.kubeconfig} --format=json > run.json 2>&1"
+          sh "gcloud run deploy ${config.serviceName} --image ${config.image} --platform kubernetes --namespace ${config.namespace} --kubeconfig ${config.kubeconfig}"
+          sh "gcloud run services describe ${config.serviceName} --platform kubernetes --kubeconfig ${config.kubeconfig} --format=json > run.json 2>&1"
         }
         else {
-          sh "gcloud beta run deploy ${config.serviceName} --image ${config.image} --allow-unauthenticated --region ${config.region} --platform managed"
-          sh "gcloud beta run services describe ${config.serviceName} --region ${config.region} --platform managed --format=json > run.json 2>&1"
+          sh "gcloud run deploy ${config.serviceName} --image ${config.image} --allow-unauthenticated --region ${config.region} --platform managed"
+          sh "gcloud run services describe ${config.serviceName} --region ${config.region} --platform managed --format=json > run.json 2>&1"
         } 
       }
       //print detail description of deployed servce
