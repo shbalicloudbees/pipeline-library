45dcac0a38d84e2175a72d0376a72be879282a75
Kurt Madel <kmadel@cloudbees.com>
2020-09-08 18:42:43 -0400

    Update gitHubDeploy.groovy

--- vars/gitHubDeploy.groovy
+++ vars/gitHubDeploy.groovy
@@ -13,8 +13,8 @@ def call(String gitHubOrg, String gitHubRepo, String deployUrl = "", String envi
         --data '{"ref":"${ref}","environment":"${environment}","required_contexts":[],"description":"CloudBees CI Deployment","transient_environment":${transientEnv},"production_environment":${productionEnv}}' \
       | jq -r '.id' | tr -d '\n' 
     """, returnStdout: true)
-    echo "GitHub deployment id: ${deploymentId}"
     env.GITHUB_DEPLOYMENT_ID = deploymentId
+    echo "GitHub deployment id: ${env.GITHUB_DEPLOYMENT_ID}"
     gitHubDeployStatus(gitHubOrg, gitHubRepo, deployUrl)
    
   }
