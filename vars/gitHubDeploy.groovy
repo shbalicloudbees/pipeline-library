e3072eff4ebc42c2269e3fc5488b47d7510f9d74
Kurt Madel <kmadel@cloudbees.com>
2020-09-08 15:48:14 -0400

    Update gitHubDeploy.groovy

--- vars/gitHubDeploy.groovy
+++ vars/gitHubDeploy.groovy
@@ -1,4 +1,4 @@
-def call(String gitHubOrg, String gitHubRepo, String deployUrl, String environment = 'staging', String credentialId = 'cloudbees-ci-workshop-github-app') {        
+def call(String gitHubOrg, String gitHubRepo, String deployUrl = "", String environment = 'staging', String credentialId = 'cloudbees-ci-workshop-github-app') {        
   withCredentials([usernamePassword(credentialsId: "${credentialId}", usernameVariable: 'GITHUB_APP', passwordVariable: 'GITHUB_ACCESS_TOKEN')]) {
    def deploymentId =  sh(script: """
       curl \
