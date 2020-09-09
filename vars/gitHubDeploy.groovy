e54139d2aa81c77e5df6bdb5a8e22ce5d455a371
Kurt Madel <kmadel@cloudbees.com>
2020-09-09 08:12:49 -0400

    Update gitHubDeploy.groovy

--- vars/gitHubDeploy.groovy
+++ vars/gitHubDeploy.groovy
@@ -10,7 +10,7 @@ def call(String gitHubOrg, String gitHubRepo, String deployUrl = "", String envi
         -H 'Accept: application/vnd.github.flash-preview+json' \
         -H 'Accept: application/vnd.github.ant-man-preview+json' \
         https://api.github.com/repos/${gitHubOrg}/${gitHubRepo}/deployments \
-        --data '{"ref":"${ref}","environment":"${environment}","required_contexts":[],"description":"CloudBees CI Deployment","transient_environment":${transientEnv},"production_environment":${productionEnv}}' \
+        --data '{"ref":"${ref}","environment":"${environment}","required_contexts":[],"description":"CloudBees CI Deployment","auto_merge":false,"transient_environment":${transientEnv},"production_environment":${productionEnv}}' \
       | jq -r '.id' | tr -d '\n' 
     """, returnStdout: true)
     env.GITHUB_DEPLOYMENT_ID = deploymentId
