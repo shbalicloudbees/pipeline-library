08a3b0447711ac96290172e9075077bae982c290
Kurt Madel <kmadel@cloudbees.com>
2020-09-10 11:55:10 -0400

    Update customYamlProps.groovy

--- vars/customYamlProps.groovy
+++ vars/customYamlProps.groovy
@@ -1,9 +1,5 @@
 // vars/customYamlProps.groovy
 def call(String file = 'cloudbees-ci.yml') {
-  //defaults from job name for Docker image
-  imageNameTag()
-  //set SHORT_COMMIT for all defineProps calls
-  gitShortCommit()
   //use the Pipeline Utility Steps plugin readYaml step to read in specified custom marker file 
   def props = readYaml file: file
   for ( e in props ) {
