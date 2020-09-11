b746b44a028f1485d1eb33e359fc84c634c42358
Kurt Madel <kmadel@cloudbees.com>
2020-09-11 15:44:32 -0400

    Update gitHubBranchProtect.groovy

--- vars/gitHubBranchProtect.groovy
+++ vars/gitHubBranchProtect.groovy
@@ -5,7 +5,7 @@ def call(String gitHubOrg, String gitHubRepo, String contexts = '["pmd","checkst
         -X PUT \
         -H 'authorization: Bearer ${GITHUB_ACCESS_TOKEN}' \
         -H 'Accept: application/vnd.github.antiope-preview+json' \
-        "https://api.github.com/repos/${gitHubOrg}/${gitHubRepo}/branches/master/protection" \
+        "https://api.github.com/repos/${gitHubOrg}/${gitHubRepo}/branches/main/protection" \
         --data '{"required_status_checks":{"strict":false,"contexts":${contexts}},"enforce_admins":null,"required_pull_request_reviews":null,"restrictions":null}'
     """
   }
