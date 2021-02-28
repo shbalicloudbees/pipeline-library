9f7a474e5d10d282e29f9941ccf44cb15400e483
Kurt Madel <kmadel@cloudbees.com>
2021-02-28 16:13:41 -0500

    Update configBundleUpdate.groovy

--- vars/configBundleUpdate.groovy
+++ vars/configBundleUpdate.groovy
@@ -1,5 +1,5 @@
 // vars/configBundleUpdate.groovy
-def call(String nameSpace = "cloudbees-core") {
+def call(String nameSpace = "sda") {
   def masterName = System.properties.'MASTER_NAME'
   def label = "kubectl"
   def podYaml = libraryResource 'podtemplates/kubectl.yml'
