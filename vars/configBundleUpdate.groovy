17ef4f1aa1e2fa26ed7c2289145a6accf89cc0a4
Sara HBALI <78555329+shbalicloudbees@users.noreply.github.com>
2021-05-18 14:03:55 +0200

    Update configBundleUpdate.groovy

--- vars/configBundleUpdate.groovy
+++ vars/configBundleUpdate.groovy
@@ -1,5 +1,5 @@
 // vars/configBundleUpdate.groovy
-def call(String nameSpace = "sda") {
+def call(String nameSpace = "cbci") {
   def masterName = System.properties.'MASTER_NAME'
   def label = "kubectl"
   def podYaml = libraryResource 'podtemplates/kubectl.yml'
