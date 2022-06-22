a3f751c4ffbfa27c8d65d8d61c89fffcb1a8028b
Sara hbali <shbali@cloudbees.com>
2022-06-22 20:19:18 +0200

    adding new script groovy

---
+++ vars/configAgent.groovy
@@ -0,0 +1,23 @@
+// vars/cloudRunDeploy.groovy
+def call(Map config) {
+        agent {
+                kubernetes {
+                        yaml '''
+kind: Pod
+spec:
+  containers:
+    - name: maven
+      image: maven:3.5.4-jdk-8-slim
+      command: ["tail", "-f", "/dev/null"]
+      imagePullPolicy: Always
+      resources:
+        requests:
+          memory: "1Gi"
+          cpu: "500m"
+        limits:
+          memory: "1Gi"
+'''
+                        defaultContainer 'maven'
+                }
+        }
+}
